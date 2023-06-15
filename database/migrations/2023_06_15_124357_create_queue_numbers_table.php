8e94f82c8b51393eafcfdf6abb1b942c5fb709da
Daniel Alvaro <danielkisaran@gmail.com>
2023-06-15 20:24:46 +0700

    adding models, migration and seeder

---
+++ database/migrations/2023_06_15_124357_create_queue_numbers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('queue_numbers', function (Blueprint $table) {
+            $table->id();
+            $table->string('code_number');
+            $table->boolean('status');
+            $table->foreignId('queue_type_id');
+            $table->timestamps();
+
+            $table->foreign('queue_type_id')->references('id')->on('queue_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('queue_numbers');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\QueueNumber;
+use App\Models\QueueType;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +23,33 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        QueueType::factory()->create([
+            'type_name' => 'A'
+        ]);
+        QueueType::factory()->create([
+            'type_name' => 'B'
+        ]);
+        QueueType::factory()->create([
+            'type_name' => 'C'
+        ]);
+
+        for($i=1; $i<=10; $i++){
+            QueueNumber::factory()->create([
+                'code_number' => 'A00'.$i,
+                'status' => 0,
+                'queue_type_id' => 1,
+            ]);
+            QueueNumber::factory()->create([
+                'code_number' => 'B00'.$i,
+                'status' => 0,
+                'queue_type_id' => 2,
+            ]);
+            QueueNumber::factory()->create([
+                'code_number' => 'C00'.$i,
+                'status' => 0,
+                'queue_type_id' => 3,
+            ]);
+        }
     }
 }
